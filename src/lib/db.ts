50398d61955a24b5ba16d2016533fe52c00e0c50
Enrique Marquez <riquemarquez802@gmail.com>
2025-04-08 01:08:16 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,14 +11,12 @@ const globalWithMongo = globalThis as typeof globalThis & {
   _mongoClientPromise?: Promise<MongoClient>;
 };
 
-let clientPromise: Promise<MongoClient>;
-
 if (!globalWithMongo._mongoClientPromise) {
   const client = new MongoClient(uri);
   globalWithMongo._mongoClientPromise = client.connect();
 }
 
-clientPromise = globalWithMongo._mongoClientPromise;
+const clientPromise = globalWithMongo._mongoClientPromise;
 
 export const getDb = async () => {
   const client = await clientPromise;
